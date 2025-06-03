ed495021ed23cbf27f6b91f4f3bebbf20fe76b8f
Mr. Z <mrz1836@users.noreply.github.com>
2025-06-03 14:53:22 -0400

    expand examples with all public functions

--- examples/examples.go
+++ examples/examples.go
@@ -1,4 +1,5 @@
-// Package main provides an example of how to use the go-countries package.
+// Package main provides examples of how to use the go-countries package.
+// Each public function is demonstrated using mostly USA, Canada and Mexico.
 package main
 
 import (
@@ -8,12 +9,27 @@ import (
 )
 
 func main() {
+	// Lookup by country name (United States)
+	usa := countries.GetByName("United States of America")
+	log.Printf("USA alpha-2: %s", usa.Alpha2)
 
-	// Get by name (show ISO 3166-1 alpha-2)
-	country := countries.GetByName("United States of America")
-	log.Println("Found: " + country.Alpha2)
+	// Lookup by alpha-2 code (Canada)
+	canada := countries.GetByAlpha2("CA")
+	log.Printf("Canada name: %s", canada.Name)
 
-	// Get by ISO 3166-1 alpha-2 (show name)
-	country = countries.GetByAlpha2("US")
-	log.Println("Found: " + country.Name)
+	// Lookup by alpha-3 code (Mexico)
+	mexico := countries.GetByAlpha3("MEX")
+	log.Printf("Mexico capital: %s", mexico.Capital)
+
+	// Lookup by numeric country code (US)
+	byCode := countries.GetByCountryCode("840")
+	log.Printf("Country for code 840: %s", byCode.Name)
+
+	// Lookup by ISO 3166-2 code (Canada)
+	byISO := countries.GetByISO31662("ISO 3166-2:CA")
+	log.Printf("Country for ISO 3166-2 'ISO 3166-2:CA': %s", byISO.Name)
+
+	// Get the full list of countries
+	all := countries.GetAll()
+	log.Printf("Total countries available: %d", len(all))
 }
